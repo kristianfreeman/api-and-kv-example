2ea1fdf9eeba58210f3defed299080dddcb5f4e1
Kristian Freeman <kristian@kristianfreeman.com>
2024-04-05 13:01:46 -0500

    2024-04-05-13:01:46

--- src/index.ts
+++ src/index.ts
@@ -3,21 +3,23 @@ type Bindings = { KV: KVNamespace };
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.get("/repos/:username", async (c) => {
-	const username = c.req.param("username");
-	const cached = await c.env.KV.get(`repos:${username}`, "json")
+  const username = c.req.param("username");
+  const cached = await c.env.KV.get(`repos:${username}`, "json");
 
-	if (cached) {
-		return c.json(cached)
-	} else {
-	  const resp = await fetch(`https://api.github.com/users/${username}/repos`, {
-	    headers: {
-	      "User-Agent": "CF Workers",
-	  	},
-    })
-	  const data = await resp.json();
-		await c.env.KV.put(`repos:${username}`, JSON.stringify(data), { expirationTtl: 60 })
-	  return c.json(data);
-	}
-})
+  if (cached) {
+    return c.json(cached);
+  } else {
+    const resp = await fetch(`https://api.github.com/users/${username}/repos`, {
+      headers: {
+        "User-Agent": "CF Workers",
+      },
+    });
+    const data = await resp.json();
+    await c.env.KV.put(`repos:${username}`, JSON.stringify(data), {
+      expirationTtl: 60,
+    });
+    return c.json(data);
+  }
+});
 
 export default app;
